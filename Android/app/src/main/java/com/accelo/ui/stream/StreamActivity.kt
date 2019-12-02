ac73be477ca3b030eb8ecd877f85a322ee7a41a0
dima <dmytro.bobyk@sjinnovation.com>
2019-12-02 18:37:44 +0200

    Moved keyboard methods to keyboard util

--- Android/app/src/main/java/com/accelo/ui/stream/StreamActivity.kt
+++ Android/app/src/main/java/com/accelo/ui/stream/StreamActivity.kt
@@ -1,13 +1,9 @@
 package com.accelo.ui.stream
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +12,7 @@ import com.accelo.databinding.ActivityStreamBinding
 import com.accelo.ui.create.CreateActivity
 import com.accelo.ui.view.ViewActivity
 import com.accelo.util.EventObserver
+import com.accelo.util.KeyboardUtil.dismissKeyboard
 import com.accelo.util.viewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerAppCompatActivity
@@ -99,15 +96,5 @@ class StreamActivity : DaggerAppCompatActivity() {
         }
 
         alertDialog.show()
-    }
-
-    private fun showKeyboard(view: View) {
-        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED)
-    }
-
-    private fun dismissKeyboard(view: View) {
-        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
