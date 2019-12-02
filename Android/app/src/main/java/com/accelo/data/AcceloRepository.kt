0b00c48aca10a02f3dbd609ca8c78066fcf4790c
dima <dmytro.bobyk@sjinnovation.com>
2019-12-02 12:38:16 +0200

    Added base view model. Refactored other view models using base view model. Added completable to saving deployment name

--- Android/app/src/main/java/com/accelo/data/AcceloRepository.kt
+++ Android/app/src/main/java/com/accelo/data/AcceloRepository.kt
@@ -4,29 +4,31 @@ import com.accelo.data.LocalDataSource
 import com.accelo.data.api.AcceloService
 import com.accelo.data.response.ActivityResponse
 import com.accelo.data.response.UserResponse
+import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
 
 /**
  * Created by Yuri Popiv on 11/26/2019.
  */
-class AcceloRepository  @Inject constructor(
+class AcceloRepository @Inject constructor(
     private val localDataSource: LocalDataSource,
     private val service: AcceloService
-){
+) {
 
     private val url = "https://${localDataSource.deployment}.api.accelo.com/"
 
-    fun saveDeploymentName(deployment: String){
-        localDataSource.deployment = deployment
-
+    fun saveDeploymentName(deployment: String): Completable {
+        return Completable.fromAction {
+            localDataSource.deployment = deployment
+        }
     }
 
-    fun getToken(code: String) : Single<UserResponse> {
+    fun getToken(code: String): Single<UserResponse> {
         return service.getToken(code)
             .doOnSuccess {
-            localDataSource.accessToken = it.accessToken
-        }
+                localDataSource.accessToken = it.accessToken
+            }
     }
 
     fun getListActivity(
