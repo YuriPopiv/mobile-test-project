2c2092b4765685831ef8a64ebc0774f101e7ffa5
yuripopiv <popiv.yuri@gmail.com>
2019-12-02 12:31:57 +0200

    Add Request and response model for Create Activity

---
+++ Android/app/src/main/java/com/accelo/data/response/CreateActivityResponse.kt
@@ -0,0 +1,19 @@
+package com.accelo.data.response
+
+import com.accelo.data.model.CreatePostData
+import com.accelo.data.model.Meta
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+/**
+ * Created by Yuri Popiv on 11/25/2019.
+ */
+data class CreateActivityResponse(
+    @SerializedName("response")
+    @Expose
+    var response: CreatePostData? = null,
+    @SerializedName("meta")
+    @Expose
+    var meta: Meta? = null
+)
